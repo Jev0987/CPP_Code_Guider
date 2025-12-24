2a3469d6c987e9a9c6e642f0b46d968747b4aea9
zhanjiewen <zhanjiewen@pudutech.com>
2025-12-24 23:32:00 +0800

    实现移除元素功能；新增找出字符串中第一个匹配项的下标功能（未实现）

--- mark_leetcode/27.移除元素.cpp
+++ mark_leetcode/27.移除元素.cpp
@@ -9,6 +9,23 @@ using std::vector;
 // @lc code=start
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {}
+    int removeElement(vector<int>& nums, int val) {
+        // 快慢指针
+        // 慢指针指向需要交换的位置
+        // 快指针往后遍历，将满足条件的值往前移
+        int n = nums.size();
+
+        int slow = 0, fast = 0;
+
+        while (fast < n) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+
+        return slow;
+    }
 };
 // @lc code=end
