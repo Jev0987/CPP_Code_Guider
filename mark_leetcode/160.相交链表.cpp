d5f06914cba2d04abb4d716337fa315c48312d0d
jev <jev0987@foxmail.com>
2026-01-12 00:02:45 +0800

    新增相交链表功能；实现链表交点查找算法

---
+++ mark_leetcode/160.相交链表.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=cpp
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// struct ListNode {
+//     int val;
+//     ListNode* next;
+//     ListNode(int x)
+//         : val(x)
+//         , next(nullptr) {}
+// };
+
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        if (headA == nullptr || headB == nullptr) return nullptr;
+        ListNode* l1 = headA;
+        ListNode* l2 = headB;
+
+        while (l1 != l2) {
+            l1 = l1 == nullptr ? headB : l1 = l1->next;
+            l2 = l2 == nullptr ? headA : l2 = l2->next;
+        }
+
+        return l1;
+    }
+};
+// @lc code=end
